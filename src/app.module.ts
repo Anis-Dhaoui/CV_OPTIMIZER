ff8a424b451153ce2903e35c2bba3d0840c38f2a
Anis Dhaoui <freeconcept6@gmail.com>
2025-02-25 13:58:25 +0100

    Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // The ConfigModule is used to manage configuration options in your application. By setting it as global, the configuration options (e.g., environment variables) are accessible across your entire application.
+    // Loads environment variables from a .env file.
+    // Setting isGlobal: true makes the config available globally (no need to re-import in other modules).
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // The MongooseModule integrates the Mongoose ODM (Object Document Mapper) with your NestJS application, allowing you to interact with a MongoDB database.
+    MongooseModule.forRoot(process.env.MONGO_URI, { dbName: 'cvoptimizer' }),
+
+    // The ServeStaticModule is used to serve static files (e.g., HTML, CSS, JavaScript files) from the specified rootPath. 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'build'),
+    }),
+
+    // RoleModule,
+    // RightsModule,
+    // GroupModule,
+    // ProgramModule,
+    // UserModule,
+    // AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //Without the following line DTO will never work
+  // Validation pipes are used to automatically validate and transform incoming data based on the DTO (Data Transfer Object) schemas you define.
+  app.useGlobalPipes(new ValidationPipe());
+
+  // CORS is a security feature that allows your application to control which origins can access your API.
+  app.enableCors(
+    {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: '*',
+      exposedHeaders: '*',
+      credentials: true,
+    }
+  );
+
+  await app.listen(3000, () => {
+    Logger.log(`Server is Running at port: 3000.`)
+  });
 }
 bootstrap();
